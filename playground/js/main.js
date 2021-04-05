6090df77c5daa00bd6ce086fabb7f496a142f5d5
Andrej <b.andrej@cumir.de>
2021-04-05 16:59:36 +0200

    - Adjusted light and shadow setup

--- playground/js/main.js
+++ playground/js/main.js
@@ -39,7 +39,6 @@ function setup_renderer() {
     renderer.outputEncoding = THREE.sRGBEncoding;
 
     renderer.shadowMap.enabled = true;
-    // renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
     document.body.appendChild(renderer.domElement);
@@ -67,59 +66,65 @@ function setup_light() {
     //hemiLight.position.set( 0, 300, 0 );
     //scene.add( hemiLight );    
 
-    const light1 = new THREE.SpotLight( 0xffffff, 50, 2, THREE.MathUtils.degToRad(13), 0.3);
+    const light1 = new THREE.SpotLight( 0xffffff, 5, 2, THREE.MathUtils.degToRad(13), 0.3);
     light1.position.set( 0.4, 0.4, 0.7 ); 
     light1.castShadow = true;
 
-    light1.shadow.bias = -0.0001;
+    light1.shadow.bias = -0.001;
 
     //Set up shadow properties for the light
     light1.shadow.mapSize.width = 2048; // default
     light1.shadow.mapSize.height = 2048; // default
     light1.shadow.camera.near = 0.5; // default
-    light1.shadow.camera.far = 3; // default
+    light1.shadow.camera.far = 2; // default
+    light1.shadow.radius = 8;
+
     //light1.shadow.camera.fov = 20; // default
     
     scene.add( light1 );
 	
 	const helper1 = new THREE.SpotLightHelper( light1 );
-    scene.add( helper1 );
+    // scene.add( helper1 );
 	
-	const light2 = new THREE.SpotLight( 0xffffff, 100, 2, THREE.MathUtils.degToRad(15) , 0.3);
+	const light2 = new THREE.SpotLight( 0xffffff, 10, 2, THREE.MathUtils.degToRad(15) , 0.3);
     light2.position.set( -0.4, 0.85, -0.2 ); 
     light2.castShadow = true;
 
-    light2.shadow.bias = -0.0001;
+    light2.shadow.bias = -0.001;
 
     //Set up shadow properties for the light
     light2.shadow.mapSize.width = 2048; // default
     light2.shadow.mapSize.height = 2048; // default
     light2.shadow.camera.near = 0.5; // default
-    light2.shadow.camera.far = 3; // default
+    light2.shadow.camera.far = 2; // default
+    light2.shadow.radius = 8;
+
     //light2.shadow.camera.fov = 25; // default
     
     scene.add( light2 );
 
     const helper2 = new THREE.SpotLightHelper( light2 );
-    scene.add( helper2 );
+    // scene.add( helper2 );
 	
-	const light3 = new THREE.SpotLight( 0xffffff, 200, 3, THREE.MathUtils.degToRad(5) , 0.5);
+	const light3 = new THREE.SpotLight( 0xffffff, 5, 3, THREE.MathUtils.degToRad(5) , 0.5);
     light3.position.set( 0.8, 0.4, -2 ); 
     light3.castShadow = true;
 
-    light3.shadow.bias = -0.0001;
+    light3.shadow.bias = -0.001;
 
     //Set up shadow properties for the light
     light3.shadow.mapSize.width = 2048; // default
     light3.shadow.mapSize.height = 2048; // default
     light3.shadow.camera.near = 0.5; // default
-    light3.shadow.camera.far = 3; // default
+    light3.shadow.camera.far = 2; // default
+    light3.shadow.radius = 8;
+
     //light2.shadow.camera.fov = 25; // default
     
     scene.add( light3 );
 
     const helper3 = new THREE.SpotLightHelper( light3 );
-    scene.add( helper3 );
+    // scene.add( helper3 );
 }
 
 function load_environment() {
@@ -135,6 +140,9 @@ function load_environment() {
                     console.info("Enable shadow cast/receive");
                     child.castShadow = true;
                     child.receiveShadow = true;
+                    if(child.material.map) child.material.map.anisotropy = 16; 
+
+                    // child.material.encoding = THREE.sRGBEncoding;
                 }
             } );
 
