ef05bfa816911696e4fd0498e14da0fd49f85902
Andrej <b.andrej@cumir.de>
2021-05-25 22:39:33 +0200

    - Adjusted layout

--- viewer/src/App.vue
+++ viewer/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    style="display: grid; grid-template-columns: 0.75fr 2.5fr 5fr; height: 100%"
+    style="display: grid; grid-template-columns: 7em 25em auto; height: 100%"
   >
     <PartList :parts="parts" />
     <PartInfoPanel :part-info="parts[selectedIndex]" />
@@ -18,56 +18,56 @@ import RenderPanel from "./components/RenderPanel.vue";
 import availableParts from "./assets/json/available_parts.json";
 
 export default {
-  name: "App",
-  components: {
-    PartInfoPanel,
-    PartList,
-    RenderPanel,
-  },
-  setup() {
-    const parts = ref(availableParts.parts);
-    const selectedIndex = ref(0); //PartList.selectedIndex;
+	name: "App",
+	components: {
+		PartInfoPanel,
+		PartList,
+		RenderPanel,
+	},
+	setup() {
+		const parts = ref(availableParts.parts);
+		const selectedIndex = ref(0); //PartList.selectedIndex;
 
-    return { parts, selectedIndex };
-  },
-  created() {
-    this.emitter.on("update_selected_model", (index) => {
-      this.selectedIndex = index;
-    });
+		return { parts, selectedIndex };
+	},
+	created() {
+		this.emitter.on("update_selected_model", (index) => {
+			this.selectedIndex = index;
+		});
 
-    this.emitter.on("switch_theme", (themeIndex) => {
-      // console.log("THEME SWITCH");
-      if (themeIndex) {
-        console.log("DARK THEME");
-        document.documentElement.setAttribute("data-theme", "dark");
-      } else {
-        console.log("LIGHT THEME");
-        document.documentElement.setAttribute("data-theme", "light");
-      }
-      //   this.selectedIndex = index;
-    });
-  },
-  //   methods: {
-  //     // _addDarkTheme() {
-  //     //   let darkThemeLinkEl = document.createElement("link");
-  //     //   darkThemeLinkEl.setAttribute("rel", "stylesheet");
-  //     //   darkThemeLinkEl.setAttribute("href", "/css/darktheme.css");
-  //     //   darkThemeLinkEl.setAttribute("id", "dark-theme-style");
-  //     //   let docHead = document.querySelector("head");
-  //     //   docHead.append(darkThemeLinkEl);
-  //     // },
-  //     // _removeDarkTheme() {
-  //     //   let darkThemeLinkEl = document.querySelector("#dark-theme-style");
-  //     //   let parentNode = darkThemeLinkEl.parentNode;
-  //     //   parentNode.removeChild(darkThemeLinkEl);
-  //     // },
-  //     // darkThemeSwitch() {
-  //     //   let darkThemeLinkEl = document.querySelector("#dark-theme-style");
-  //     //   if (!darkThemeLinkEl) {
-  //     //     this._addDarkTheme()
-  //     //   } else {
-  //     //     this._removeDarkTheme()
-  //     //   }
-  //}
+		this.emitter.on("switch_theme", (themeIndex) => {
+			// console.log("THEME SWITCH");
+			if (themeIndex) {
+				console.log("DARK THEME");
+				document.documentElement.setAttribute("data-theme", "dark");
+			} else {
+				console.log("LIGHT THEME");
+				document.documentElement.setAttribute("data-theme", "light");
+			}
+			//   this.selectedIndex = index;
+		});
+	},
+	//   methods: {
+	//     // _addDarkTheme() {
+	//     //   let darkThemeLinkEl = document.createElement("link");
+	//     //   darkThemeLinkEl.setAttribute("rel", "stylesheet");
+	//     //   darkThemeLinkEl.setAttribute("href", "/css/darktheme.css");
+	//     //   darkThemeLinkEl.setAttribute("id", "dark-theme-style");
+	//     //   let docHead = document.querySelector("head");
+	//     //   docHead.append(darkThemeLinkEl);
+	//     // },
+	//     // _removeDarkTheme() {
+	//     //   let darkThemeLinkEl = document.querySelector("#dark-theme-style");
+	//     //   let parentNode = darkThemeLinkEl.parentNode;
+	//     //   parentNode.removeChild(darkThemeLinkEl);
+	//     // },
+	//     // darkThemeSwitch() {
+	//     //   let darkThemeLinkEl = document.querySelector("#dark-theme-style");
+	//     //   if (!darkThemeLinkEl) {
+	//     //     this._addDarkTheme()
+	//     //   } else {
+	//     //     this._removeDarkTheme()
+	//     //   }
+	//}
 };
 </script>
